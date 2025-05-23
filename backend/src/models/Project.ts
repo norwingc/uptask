cca2e81564a74b2e15ae6e2ab581bc83a313113f
Norwin Guerreo <norwingcruz@gmail.com>
2025-05-23 00:03:45 -0500

    Agregar campos de tareas y estado al modelo de proyecto y tarea, mejorando la relación entre ambos modelos y la gestión de estados en TypeScript.

--- backend/src/models/Project.ts
+++ backend/src/models/Project.ts
@@ -1,16 +1,22 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
 
 export interface IProject extends Document {
     projectName: string;
     clientName: string;
     description: string;
+    tasks: PopulatedDoc<ITask & Document>[];
 }
 
-const ProjectSchema: Schema = new Schema({
-    projectName: { type: String, trim: true, required: true },
-    clientName: { type: String, trim: true, required: true },
-    description: { type: String, trim: true, required: true },
-});
+const ProjectSchema: Schema = new Schema(
+    {
+        projectName: { type: String, trim: true, required: true },
+        clientName: { type: String, trim: true, required: true },
+        description: { type: String, trim: true, required: true },
+        tasks: [{ type: Types.ObjectId, ref: "Task", required: true }],
+    },
+    { timestamps: true }
+);
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema);
 
--- backend/src/models/Task.ts
+++ backend/src/models/Task.ts
@@ -1,14 +1,35 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+const taskStatus = {
+    PENDING: "pending",
+    ON_HOLD: "onHold",
+    IN_PROGRESS: "inProgress",
+    UNDER_REVIEW: "underReview",
+    COMPLETED: "completed",
+} as const;
+
+export type TaskStatus = (typeof taskStatus)[keyof typeof taskStatus];
 
 export interface ITask extends Document {
     name: string;
     description: string;
+    project: Types.ObjectId;
+    status: TaskStatus;
 }
 
-const TaskSchema: Schema = new Schema({
-    name: { type: String, trim: true, required: true },
-    description: { type: String, trim: true, required: true },
-});
+const TaskSchema: Schema = new Schema(
+    {
+        name: { type: String, trim: true, required: true },
+        description: { type: String, trim: true, required: true },
+        project: { type: Types.ObjectId, ref: "Project", required: true },
+        status: {
+            type: String,
+            enum: Object.values(taskStatus),
+            default: taskStatus.PENDING,
+        },
+    },
+    { timestamps: true }
+);
 
 const Task = mongoose.model<ITask>("Task", TaskSchema);
 
